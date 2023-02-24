1c8b386af36d97001b8c558da38694d77939f1f5
Sam Verdejo <verdejos@sheridancollege.ca>
2023-02-23 19:50:34 -0500

    new Course class

---
+++ src/studentclass/Course.java
@@ -0,0 +1,18 @@
+package studentclass;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sverdejo
+ */
+public class Course {
+    private final String name;
+    
+    Course(String name) {
+        this.name = name;
+    }
+}
