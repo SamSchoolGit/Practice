9902508cf085df7804d03c7d999f83a36b671f9a
Sam Verdejo <verdejos@sheridancollege.ca>
2023-02-23 20:02:35 -0500

    created teacher class

---
+++ src/studentclass/Teacher.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentclass;
+
+/**
+ *
+ * @author sverdejo
+ */
+public class Teacher {
+    String name;
+    
+    Teacher(String name) {
+        this.name = name;
+    }
+}
