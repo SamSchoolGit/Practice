fdeeca938cbae1372772373122174f06bf0569a4
SamSchoolGit <123758624+SamSchoolGit@users.noreply.github.com>
2023-02-23 19:51:35 -0500

    Added id

--- src/studentclass/Course.java
+++ src/studentclass/Course.java
@@ -11,8 +11,10 @@ package studentclass;
  */
 public class Course {
     private final String name;
+    private final String id;
     
-    Course(String name) {
+    Course(String name, String id) {
         this.name = name;
+        this.id = id;
     }
 }
